7477c66ff51486790b0f3fd6e21da41ea4bd4937
shalabhraj1990 <shalabhrajetc@gmail.com>
2021-11-14 08:19:46 +0530

    added fake test cases

---
+++ file.management.fake.spec.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const fs = require("fs");
+const proxyquire = require("proxyquire");
+const fileManagement = require("./file.management");
+
+describe("File Management", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+  describe("when creating a new file", () => {
+    it.skip("should call writeFileSync", () => {
+      const writeFake = sinon.fake(fs.writeFileSync);
+      sinon.replace(fs, "writeFileSync", writeFake);
+      const fileManagement = proxyquire("./file.management", { fs });
+      fileManagement.createFile("test.txt");
+      expect(writeFake.calledWith("./data/test.txt", "")).to.be.true;
+    });
+    it("should thorw an exception when the file already exists", () => {
+      const writeFake = sinon.fake.throws(new Error());
+      sinon.replace(fs, "writeFileSync", writeFake);
+      const fileManagement = proxyquire("./file.management", { fs });
+      expect(() => {
+        fileManagement.createFile("test.txt").to.throws;
+      });
+    });
+    it("getAllFiles should return a list of files", () => {
+      const readFake = sinon.fake.yields(null, ["test.txt"]);
+      sinon.replace(fs, "writeFileSync", readFake);
+      const fileManagement = proxyquire("./file.management", { fs });
+      fileManagement.getAllFiles((err, data) => {
+        expect(data).to.eql(["test.txt"]);
+      });
+    });
+  });
+});
